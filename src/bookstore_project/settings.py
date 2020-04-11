3f367a8d6ecc1dad5abb32db5c45d0ff327a52d3
Carlos Albaladejo <carlos.alba.padilla@gmail.com>
2020-04-11 13:52:54 +0200

    About page and static

--- src/bookstore_project/settings.py
+++ src/bookstore_project/settings.py
@@ -122,7 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static Conf
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
--- src/pages/views.py
+++ src/pages/views.py
@@ -2,8 +2,12 @@
     Views para pages
 """
 
-from django.shortcuts import render
 from django.views.generic import TemplateView
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
