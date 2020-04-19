7eb798ba0bfc4aa5e160383a7e86b2bf104d994f
Carlos Albaladejo <carlos.alba.padilla@gmail.com>
2020-04-19 19:37:05 +0200

    Added Books Detail

--- src/books/tests.py
+++ src/books/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='Moby-Dick',
+            author='Herman Melville',
+            price=25.00,
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'Moby-Dick')
+        self.assertEqual(f'{self.book.author}', 'Herman Melville')
+        self.assertEqual(self.book.price, 25.00)
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Moby-Dick')
+        self.assertTemplateUsed(response, 'books/book_list.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Moby-Dick')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
